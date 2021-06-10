2782089f068eb74ea66595580169bf0d4fc804d4
Klaymant <c.gauthier94250@gmail.com>
2021-06-10 22:22:51 +0200

    Configuring authentication

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    /**
+    * @Route("/login", name="login_check", methods={"POST"})
+    */
+    public function loginCheck() {
+        $user = $this->getUser();
+        return $this->json(array(
+            'username' => $user->getUsername(),
+            'roles' => $user->getRoles()
+        ));
+    }
+}
